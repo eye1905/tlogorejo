9ccd68d170cdc3f9448111da1589d2702b8df34b
Arif-PC\Arif <ariefmanafda@gmail.com>
2018-04-05 09:15:37 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -19,7 +19,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 //$config['base_url'] = '';
 
-$config['base_url']    = 'http://localhost:8080/tlogorejo/';
+$config['base_url']    = 'http://localhost/tlogorejo/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/admin_template.php
+++ application/views/admin_template.php
@@ -12,37 +12,37 @@ if ($this->session->userdata('masuk') == TRUE) {
   <title>Admin Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="<?php echo base_url('assets/adminlte/bootstrap/dist/css/bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/adminlte/font-awesome/css/font-awesome.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/adminlte/Ionicons/css/ionicons.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/MyStyle.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/dist/css/MyStyle.min.css">
 
-  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/skins/skin-black.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/dist/css/skins/skin-black.min.css">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <!-- DataTables -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/adminlte/datatables.net-bs/css/dataTables.bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/datatables.net-bs/css/dataTables.bootstrap.min.css">
   <!-- Customize CSS -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/custom/admin-styles.css') ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/custom/admin-styles.css">
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
   <!-- REQUIRED JS SCRIPTS -->
   <!-- jQuery 3 -->
-  <script src="<?php echo base_url('assets/adminlte/jquery/dist/jquery.min.js') ?>"></script>
+  <script src="<?php echo base_url() ?>assets/adminlte/jquery/dist/jquery.min.js"></script>
   <!-- Bootstrap 3.3.7 -->
-  <script src="<?php echo base_url('assets/adminlte/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+  <script src="<?php echo base_url() ?>assets/adminlte/bootstrap/dist/js/bootstrap.min.js"></script>
   <!-- AdminLTE App -->
-  <script src="<?php echo base_url('assets/dist/js/adminlte.min.js') ?>"></script>
+  <script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js"></script>
   <!-- CK Editor -->
-  <script src="<?php echo base_url('assets/adminlte/ckeditor/ckeditor.js') ?>"></script>
+  <script src="<?php echo base_url() ?>assets/adminlte/ckeditor/ckeditor.js"></script>
   <!-- DataTables -->
-  <script src="<?php echo base_url('assets/adminlte/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
-  <script src="<?php echo base_url('assets/adminlte/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script>
+  <script src="<?php echo base_url() ?>assets/adminlte/datatables.net/js/jquery.dataTables.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/adminlte/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
   <!-- Optionally, you can add Slimscroll and FastClick plugins.
        Both of these plugins are recommended to enhance the
        user experience. -->
